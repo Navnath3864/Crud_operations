92afe8ef1f6f17cfb3e728cf4f40c1e83fe1244e
Navnath3864 <nagutte0@gmail.com>
2025-01-16 17:47:26 +0530

    add sop statement in main class by navnath

--- CrudDemo/src/main/java/com/app/CrudDemoApplication.java
+++ CrudDemo/src/main/java/com/app/CrudDemoApplication.java
@@ -9,6 +9,7 @@ public class CrudDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrudDemoApplication.class, args);
 		System.out.println("Crud operation app : 8080");
+		System.out.println("Check Pull");
 	}
 
 }
