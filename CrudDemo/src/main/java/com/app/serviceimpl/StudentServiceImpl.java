16f571e87ade55ee44b0899c7a86315a2eb773d3
Navnath3864 <nagutte0@gmail.com>
2025-01-16 16:51:50 +0530

    commit-2 Add all classes and interface by Navnath

---
+++ CrudDemo/src/main/java/com/app/serviceimpl/StudentServiceImpl.java
@@ -0,0 +1,10 @@
+package com.app.serviceimpl;
+
+import org.springframework.stereotype.Service;
+
+import com.app.service.StudentService;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+
+}
